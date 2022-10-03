eb63f5eac6004ba6e5113ef357b0d4c4fea0ba24
hyun3167 <hyun3167@gmail.com>
2022-10-04 03:31:56 +0900

    20221004

---
+++ android/app/src/main/java/com/example/myapplication/User.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+public class User {
+    private String profile;
+    private String id;
+    private int pw;
+    private String userName;
+
+    public User(){}
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getPw() {
+        return pw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
