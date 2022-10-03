984214e06dd9a9e1aacfbf0431f034a46575dd44
hyun3167 <hyun3167@gmail.com>
2022-10-03 09:01:03 +0900

    re

---
+++ android/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
