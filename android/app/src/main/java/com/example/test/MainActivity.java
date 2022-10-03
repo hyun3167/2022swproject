5537a2f5341d029f8f37790f9cdf8fa1af9f112a
hyun3167 <hyun3167@gmail.com>
2022-10-03 22:29:05 +0900

    20221003

--- android/app/src/main/java/com/example/test/MainActivity.java
+++ android/app/src/main/java/com/example/test/MainActivity.java
@@ -1,19 +1,67 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.myapplication.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<User> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView); // 아이디 연결
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트
+
+        database = FirebaseDatabase.getInstance(); // 파이어 베이스 데이터 베이스 연동
+
+        databaseReference = database.getReference("User");
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //파이어 베이스 데이터 베이스의 데이터를 받아오는곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지 않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){ //  반복문으로 데이터 List를 추출
+                    User user = snapshot.getValue(User.class); // 만들어 뒀던 User 객체에 데이터 담기
+                    arrayList.add(user); // 담을 데이터들을 배열 리스트에 넣고 recyclerView로 보낼 준비
+                }
+                adapter.notifyDataSetChanged();//리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //DB를 가져 오던 중 에러 발생 시
+                Log.e("mainActivity", String.valueOf(databaseError.toException())); //에러 문 출력
+
+            }
+        });
+
+        adapter = new CustomAdapter(arrayList, this);
+        recyclerView.setAdapter(adapter); //리사이클러 뷰에 어댑터 연결
+
     }
 }
